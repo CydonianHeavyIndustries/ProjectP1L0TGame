15377abc28c9b9e62cdf3cce734cecd2e9d7637b
Beurkson <cydonianheavyindustries@gmail.com>
2026-02-02 16:19:27 -0500

    Update Vite/Electron toolchain and add debug runner

---
+++ run_debug.bat
@@ -0,0 +1,18 @@
+﻿@echo off
+setlocal
+
+set ELECTRON_ENABLE_LOGGING=1
+set ELECTRON_ENABLE_STACK_DUMPING=1
+
+cd /d "%~dp0Launcher"
+if not exist "node_modules" (
+  echo Installing dependencies...
+  call npm install
+)
+echo Starting launcher (debug)...
+call npm run dev
+
+echo.
+echo Launcher exited with code %errorlevel%.
+pause
+endlocal
