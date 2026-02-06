38c2c773ed006370f44ac7d94902089cecc9819e
Beurkson <cydonianheavyindustries@gmail.com>
2026-02-06 15:00:52 -0500

    Fix gameplay resources and log overlay, bump version

--- scripts/core_systems/logging/LogOverlay.gd
+++ scripts/core_systems/logging/LogOverlay.gd
@@ -4,18 +4,19 @@ extends CanvasLayer
 @export var visible_on_start := true
 
 @onready var log_label: RichTextLabel = $Panel/Margins/LogLabel
+@onready var logger: Node = get_node_or_null("/root/Logger")
 
 func _ready() -> void:
 	visible = visible_on_start
-	if Logger:
-		Logger.log_added.connect(_on_log_added)
+	if logger:
+		logger.log_added.connect(_on_log_added)
 		_refresh()
 
 func _on_log_added(_line: String) -> void:
 	_refresh()
 
 func _refresh() -> void:
-	if not Logger:
+	if not logger:
 		return
-	var lines = Logger.get_recent_lines(max_lines)
+	var lines = logger.get_recent_lines(max_lines)
 	log_label.text = "\n".join(lines)
